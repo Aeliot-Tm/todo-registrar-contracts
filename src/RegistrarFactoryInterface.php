8b46f0d520748d62725635259c71b6c8954bbd81
Anatoliy Melnikov <5785276@gmail.com>
2026-01-02 20:32:10 +0300

    Fix code style

--- src/RegistrarFactoryInterface.php
+++ src/RegistrarFactoryInterface.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the TODO Registrar Contracts project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Aeliot\TodoRegistrarContracts;
 
 use Aeliot\TodoRegistrarContracts\Exception\InvalidConfigException;
@@ -10,14 +19,14 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 interface RegistrarFactoryInterface
 {
     /**
-     * @param array<string,mixed> $config   Only array to not rise up set of interfaces. Transform it inside factory.
+     * @param array<string,mixed> $config Only array to not rise up set of interfaces. Transform it inside factory.
      * @param ValidatorInterface $validator Validator will be passed but I don't want enfoce using of it.
      *                                      Get it from function arguments.
-     * <pre>
-     *     $validator = func_get_arg(1);
-     * </pre>
+     *                                      <pre>
+     *                                      $validator = func_get_arg(1);
+     *                                      </pre>
      *
      * @throws InvalidConfigException
      */
-    public function create(array $config/*, ValidatorInterface $validator */): RegistrarInterface;
+    public function create(array $config/* , ValidatorInterface $validator */): RegistrarInterface;
 }
