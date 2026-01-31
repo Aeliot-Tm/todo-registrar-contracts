63c19caa7c07e116ef0915db4f7736e7337d335e
Anatoliy Melnikov <5785276@gmail.com>
2026-01-31 17:52:28 +0300

    Add config of issue key position

---
+++ src/IssueKeyPositionConfigInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TODO Registrar Contracts project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\TodoRegistrarContracts;
+
+interface IssueKeyPositionConfigInterface
+{
+    public function getIssueKeyPosition(): ?string;
+
+    /**
+     * @return string[]
+     */
+    public function getSummarySeparators(): array;
+}
